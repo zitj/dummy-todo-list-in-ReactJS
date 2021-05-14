5876c4d21bbce72646892537289ccdc5bdba023e
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-05-14 11:02:43 +0200

    Add show/hide edit task input on click

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Task.css';
 
 const Task = (props) => {
+    let [showEditTaskInput, setShowEditTaskInput] = useState(false);
+    let [editBtn, setEditBtn] = useState('✏️');
+
     const deleteDataHandler = (event) => {
         props.onDelete(props.id);
     };
+    const editDataHandler = (event) => {
+        if (!showEditTaskInput) {
+            setEditBtn('✔️');
+        } else {
+            setEditBtn('✏️');
+        }
+        return showEditTaskInput
+            ? setShowEditTaskInput(false)
+            : setShowEditTaskInput(true);
+    };
+
+    const editTaskInput = <input type="text" className="editTaskInput" />;
+
     return (
         <li>
             {props.name}
 
             <div className="buttons">
-                <button className="editBtn">✏️</button>
+                <button className="editBtn" onClick={editDataHandler}>
+                    {editBtn}
+                </button>
                 <button className="deleteBtn" onClick={deleteDataHandler}>
                     ❌
                 </button>
             </div>
+            {showEditTaskInput ? editTaskInput : null}
         </li>
     );
 };
